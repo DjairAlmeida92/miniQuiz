747026778bfe1e51d977f49e5571447addec4a24
djair <djaptbr10@gmail.com>
2022-11-08 18:16:08 +0000

    put in the questions in my aplication

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("fazendo teste");
+const question = document.querySelector(".questions");
+const answers = document.querySelector(".answers");
+const spnQtd = document.querySelector(".spnQtd");
+const textFinish = document.querySelector(".finish span");
+const content = document.querySelector(".content");
+const contentFinish = document.querySelector(".finish");
+const btnRestart = document.querySelector(".finish button");
