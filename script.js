3de599a9af612e714d0ee40ef3e1f324c332d750
djair <djaptbr10@gmail.com>
2022-11-09 23:20:13 +0000

    testing JS

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const textFinish = document.querySelector(".finish span");
 const content = document.querySelector(".content");
 const contentFinish = document.querySelector(".finish");
 const btnRestart = document.querySelector(".finish button");
+
+import questions from "./questions";
+
+let currentIndex = 0;
+let questionsCorrect = 0;
+
+function loadQuestion() {
+    spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
+    const item = questions[currentIndex];
+    answers.innerHTML = "";
+    questions.innerHTML = item.question;
+}
+
+loadQuestion();
